90f84e68849ca22a5d0dc19ed3cfa2bd6f86c7de
Haldan Koffi <e.haldankoffi@gmail.com>
2024-10-03 11:35:26 +0200

    debut projet2

---
+++ projet2.ts
@@ -0,0 +1,17 @@
+function analyse_texte(texte: string) : any[] {
+    
+    let nombre_mots: number = [];
+    let nombre_phrases: number = [];
+    
+    const lettres_min: string ="abcdefghijklmnopqrstuvwxyz";
+    const lettres_maj: string ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    
+    let nombre_mot: number = 0;
+    let nombre_phrase: number = 0;
+    let freq_lettre: number = 0;
+    //let mot_plus_utilise
+
+
+    
+
+}
